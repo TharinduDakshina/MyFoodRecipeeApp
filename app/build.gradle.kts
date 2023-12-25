e3a5d5d7bc2e7c488d0e43e70758473d8a552718
TharinduDakshina <tharindudakshina527@gmail.com>
2023-12-25 22:55:43 +0530

    update gradel

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,17 @@ android {
 
 dependencies {
 
+    //Compose viewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
 
+    //Network calls
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+
+    //Json to kotlin object mapping
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    //Image loading
+    implementation("io.coil-kt:coil-compose:2.4.0")
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
